f4fdfcca0eba6bc8d5077b88d91557a639a72c29
Kristoffer Dalby <kradalby@kradalby.no>
2019-01-21 10:13:08 +0000

    adds csrf-xhr to automagically add csrf token rails style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,34 @@
 'use strict'
 
+// Patch AJAX request with CSRF token Rails style
+require('csrf-xhr')
 require('./styles.scss')
 
 const {Elm} = require('./Main')
 
-const storageKey = 'store'
-const flags = localStorage.getItem(storageKey)
+// const storageKey = 'store'
+// const flags = localStorage.getItem(storageKey)
+const flags = null
 let app = Elm.Main.init({flags: flags})
 
-app.ports.storeCache.subscribe(function (val) {
-  if (val === null) {
-    localStorage.removeItem(storageKey)
-  } else {
-    localStorage.setItem(storageKey, JSON.stringify(val))
-  }
-
-  // Report that the new session was stored succesfully.
-  setTimeout(function () { app.ports.onStoreChange.send(val) }, 0)
-})
+// app.ports.storeCache.subscribe(function (val) {
+//   if (val === null) {
+//     localStorage.removeItem(storageKey)
+//   } else {
+//     localStorage.setItem(storageKey, JSON.stringify(val))
+//   }
+//
+//   // Report that the new session was stored succesfully.
+//   setTimeout(function () { app.ports.onStoreChange.send(val) }, 0)
+// })
 
 // Whenever localStorage changes in another tab, report it if necessary.
-window.addEventListener('storage', function (event) {
-  if (event.storageArea === localStorage && event.key === storageKey) {
-    app.ports.onStoreChange.send(event.newValue)
-  }
-}, false)
-
-app.ports.toJs.subscribe(data => {
-  console.log(data)
-})
+// window.addEventListener('storage', function (event) {
+//   if (event.storageArea === localStorage && event.key === storageKey) {
+//     app.ports.onStoreChange.send(event.newValue)
+//   }
+// }, false)
+//
+// app.ports.toJs.subscribe(data => {
+//   console.log(data)
+// })
